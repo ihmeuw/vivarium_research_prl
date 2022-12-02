fd0d6ffcd387858962178f753222ca0a5960cf5a
Nathaniel Blair-Stahn <ndbs@uw.edu>
2022-12-01 16:20:00 -0800

    add keyboard corruption

--- src/vivarium_research_prl/noise/corruption.py
+++ src/vivarium_research_prl/noise/corruption.py
@@ -68,3 +68,41 @@ def phonetic_corrupt(truth, corrupted_pr):
             err += truth[i:(i+1)]
             i += 1
     return err
+
+# Keyboard corruption
+
+df_qwerty = pd.read_csv('qwerty-keyboard.csv', skiprows=[0,1], header=None)
+
+qwerty_error_dict = {}
+for i in df_qwerty.index:
+    for j in df_qwerty.columns:
+        val = df_qwerty.loc[i,j]
+        if str(val) != 'nan' and val != '#':
+            nbrs = []
+            for di in [-1,0,1]:
+                for dj in [-1,0,1]:
+                    if di != 0 or dj != 0: # only actual nbrs, not val itself
+                        if i+di in df_qwerty.index and j+dj in df_qwerty.columns:
+                            nbr_val = df_qwerty.loc[i+di, j+dj]
+                            if nbr_val != '#':
+                                nbrs.append(nbr_val)
+            qwerty_error_dict[val] = nbrs
+
+def keyboard_corrupt(truth, corrupted_pr, addl_pr):
+    err = ''
+    i = 0
+    while i < len(truth):
+        error_introduced = False
+        for token_length in [1]:
+            token = truth[i:(i+token_length)]
+            if token in phonetic_error_dict and not error_introduced:
+                if np.random.uniform() < corrupted_pr:
+                    err += np.random.choice(qwerty_error_dict[token])
+                    if np.random.uniform() < addl_pr:
+                        err += token
+                    i += token_length
+                    error_introduced = True
+        if not error_introduced:
+            err += truth[i:(i+1)]
+            i += 1
+    return err
