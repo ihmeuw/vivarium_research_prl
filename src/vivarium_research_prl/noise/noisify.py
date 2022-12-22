6e226b2fd52eab0bfc9d92bafdfb966dd12bda3e
Nathaniel Blair-Stahn <ndbs@uw.edu>
2022-12-22 15:56:54 -0800

    fix bug with initializing empty dict as a default param

--- src/vivarium_research_prl/noise/noisify.py
+++ src/vivarium_research_prl/noise/noisify.py
@@ -4,7 +4,7 @@ Module to apply noise to dataframe columns.
 from . import corruption, fake_names
 
 def apply_noise_function_to_column(
-    df, colname, row_prob, rng, noise_function, args=(), kwargs={},
+    df, colname, row_prob, rng, noise_function, args=None, kwargs=None,
     vectorized=True, share_random_state=True, inplace=False
 ):
     """Apply a noise function that operates on scalars or on pandas Series
@@ -12,9 +12,14 @@ def apply_noise_function_to_column(
     """
     if not inplace:
         df = df.copy()
+    if args is None:
+        args = ()
+    if kwargs is None:
+        kwargs = {}
     corrupted = rng.random(len(df)) < row_prob
     if share_random_state:
         kwargs['random_state'] = rng
+    print(noise_function, args, kwargs)
     if vectorized:
         df.loc[corrupted, colname] = noise_function(df.loc[corrupted, colname], *args, **kwargs)
     else:
@@ -32,7 +37,7 @@ def apply_noise_to_column(df, colname, rng, function_args_dict, inplace=False):
         noise_func = getattr(globals()[module_name], funcname)
         apply_noise_function_to_column(
             df, colname, params['row_prob'], rng, noise_func,
-            params.get('args', ()), prams.get('kwargs', {}),
+            params.get('args'), prams.get('kwargs'),
             params.get('vectorized', True), params.get('share_random_state', True),
             inplace=True
         )
