187982a3797c72fcbea96dea582d61b864762be3
Nathaniel Blair-Stahn <ndbs@uw.edu>
2022-12-22 14:31:46 -0800

    add function to apply multiple noise functions to a column

--- src/vivarium_research_prl/noise/noisify.py
+++ src/vivarium_research_prl/noise/noisify.py
@@ -1,16 +1,17 @@
 """
 Module to apply noise to dataframe columns.
 """
+from . import corruption, fake_names
 
 def apply_noise_function_to_column(
     df, colname, row_prob, rng, noise_function, args=(), kwargs={},
     vectorized=True, share_random_state=True, inplace=False
 ):
-    if not inplace:
-        df = df.copy()
     """Apply a noise function that operates on scalars or on pandas Series
     to a fraction of rows in a single column of a dataframe.
     """
+    if not inplace:
+        df = df.copy()
     corrupted = rng.random(len(df)) < row_prob
     if share_random_state:
         kwargs['random_state'] = rng
@@ -21,3 +22,23 @@ def apply_noise_function_to_column(
             lambda x: noise_function(x, *args, **kwargs))
     if not inplace:
         return df
+
+def apply_noise_to_column(df, colname, rng, function_args_dict, inplace=False):
+    """Apply multiple noise functions to a column, using parameters specified in a dictionary."""
+    if not inplace:
+        df = df.copy()
+    for funckey, params in function_args_dict.items():
+        module_name, funcname = funckey.split('.')
+        noise_func = getattr(globals()[module_name], funcname)
+        apply_noise_function_to_column(
+            df, colname, params['row_prob'], rng, noise_func,
+            params.get('args', ()), prams.get('kwargs', {}),
+            params.get('vectorized', True), params.get('share_random_state', True),
+            inplace=True
+        )
+    if not inplace:
+        return df
+
+def _locals_globals_test():
+    a,b,c=1,2,3 # This is all that's in locals
+    return locals(), globals()
