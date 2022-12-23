fa8e4d9a4449c5571f14fea13f21e370c6a035e1
Nathaniel Blair-Stahn <ndbs@uw.edu>
2022-12-23 13:57:13 -0800

    copy census-noising function from notebook

--- src/vivarium_research_prl/find_kids/noisify_data.py
+++ src/vivarium_research_prl/find_kids/noisify_data.py
@@ -4,6 +4,67 @@ Module for adding noise to the decennial census and WIC data before linking.
 import numpy as np
 from ..noise import noisify, corruption, fake_names
 
+def add_noise_to_census(df_census, random_state):
+    rng = np.random.default_rng(random_state)
+    # Copy the dataframe since we're going to alter it
+    df_census = df_census.copy()
+
+    # First name
+    apply_noise_to_column(
+        df_census, 'first_name', 0.01, rng,
+        corruption.keyboard_corrupt, (0.05, 0.02), inplace=True)
+    apply_noise_to_column(
+        df_census, 'first_name', 0.001, rng,
+        corruption.phonetic_corrupt, (0.05,), inplace=True)
+
+    # Last name
+    apply_noise_to_column(
+        df_census, 'last_name', 0.01, rng,
+        corruption.keyboard_corrupt, (0.05, 0.02), inplace=True)
+    apply_noise_to_column(
+        df_census, 'last_name', 0.001, rng,
+        corruption.phonetic_corrupt, (0.05,), inplace=True)
+
+    # Date of birth
+    apply_vectorized_noise_to_column(
+        df_census, 'date_of_birth', 0.005, rng, swap_month_day, inplace=True)
+    apply_noise_to_column(
+        df_census, 'date_of_birth', 0.01, rng,
+        corruption.keyboard_corrupt, (0.05, 0.02), inplace=True)
+
+    # Zipcode
+#     apply_noise_to_column(
+#         df_census, 'zipcode', 0.01, rng,
+#         miswrite_zipcode, (0.1, 0.2, rng), inplace=True)
+    apply_vectorized_noise_to_column(
+        df_census, 'zipcode', 0.01, rng,
+        miswrite_zipcode_vec, (0.1, 0.2, rng), inplace=True)
+    apply_noise_to_column(
+        df_census, 'zipcode', .01, rng,
+        corruption.keyboard_corrupt, (0.05, 0.02), inplace=True)
+
+    # Address
+    apply_noise_to_column(
+        df_census, 'address', 0.002, rng,
+        corruption.keyboard_corrupt, (0.05, 0.02), inplace=True)
+    apply_noise_to_column(
+        df_census, 'address', 0.005, rng,
+        corruption.phonetic_corrupt, (0.05,), inplace=True)
+
+    # Sex
+    ...
+
+    # Age
+    ...
+
+    # Race/Ethnicity
+    ...
+
+    # Middle initial
+    ...
+
+    return df_census
+
 def add_noise_to_wic(df_wic, random_state):
     rng = np.random.default_rng(random_state)
     # Copy the dataframe since we're going to alter it
