328bc4a907b5a467d4022270fcf3236ae5d9bf04
Nathaniel Blair-Stahn <ndbs@uw.edu>
2023-04-14 15:09:57 -0700

    change row_noise_level to probability

--- src/vivarium_research_prl/alpha.py
+++ src/vivarium_research_prl/alpha.py
@@ -1,6 +1,8 @@
 """Module with utility functions for alpha testing the Pseudopeople package.
 """
 
+from pseudopeople.configuration import get_configuration
+
 def percent_different_in_columns(df1, df2):
     return 100 * (df1 != df2).sum() / len(df1)
 
@@ -13,3 +15,11 @@ def compare_columns(df1, df2, colname, notna=False):
         return df1[colname].loc[notna].compare(df2[colname].loc[notna])
     else:
         return df1[colname].compare(df2[colname])
+
+def get_zero_column_noise_config():
+    config = get_configuration()
+    for dataset_config in config.values():
+        for column_config in dataset_config['column_noise'].values():
+            for noise_config in column_config.values():
+                noise_config['probability'] = 0
+    return config
