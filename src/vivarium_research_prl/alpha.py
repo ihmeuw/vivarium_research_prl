042ffb245f69318fbfa87aabed8c4af45c029488
Nathaniel Blair-Stahn <ndbs@uw.edu>
2023-04-14 14:44:10 -0700

    add compare_columns function

--- src/vivarium_research_prl/alpha.py
+++ src/vivarium_research_prl/alpha.py
@@ -6,3 +6,10 @@ def percent_different_in_columns(df1, df2):
 
 def percent_of_rows_with_difference(df1, df2):
     return 100 * (df1 != df2).any(axis=1).sum() / len(df1)
+
+def compare_columns(df1, df2, colname, notna=False):
+    if notna:
+        notna = df1[colname].notna() & df2[colname].notna()
+        return df1[colname].loc[notna].compare(df2[colname].loc[notna])
+    else:
+        return df1[colname].compare(df2[colname])
