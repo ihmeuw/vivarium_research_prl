7d46024a07adcbb4872c5a44537999693815d2b8
Nathaniel Blair-Stahn <ndbs@uw.edu>
2023-05-03 15:52:31 -0700

    update config keys for zero noise

--- src/vivarium_research_prl/alpha.py
+++ src/vivarium_research_prl/alpha.py
@@ -1,7 +1,7 @@
 """Module with utility functions for alpha testing the Pseudopeople package.
 """
 
-from pseudopeople.configuration import get_configuration
+from pseudopeople import get_config
 
 def percent_different_in_columns(df1, df2):
     return 100 * (df1 != df2).sum() / len(df1)
@@ -19,16 +19,14 @@ def compare_columns(df1, df2, colname, notna=False):
 def get_zero_noise_config(row_or_col='both'):
     if row_or_col not in ['row', 'column', 'both']:
         raise ValueError("row_or_col must be 'row', 'column', or 'both'")
-    config = get_configuration()
+    config = get_config()
     for dataset_config in config.values():
         if row_or_col in ['row', 'both']:
             for row_noise_config in dataset_config['row_noise'].values():
-                row_noise_config['probability'] = 0
+                row_noise_config['row_probability'] = 0
         if row_or_col in ['column', 'both']:
             for column_config in dataset_config['column_noise'].values():
                 for noise_config in column_config.values():
-                    if 'probability' in noise_config:
-                        noise_config['probability'] = 0
-                    elif 'cell_probability' in noise_config:
+                    if 'cell_probability' in noise_config:
                         noise_config['cell_probability'] = 0
     return config
